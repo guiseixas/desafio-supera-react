fc2615e35607190ec0e17b989769c1d1078ae488
guilherme <guilherme.santos@dellead.com>
2023-07-12 18:44:01 -0300

    created footer and header components and home view

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import '../components/Footer.css'
+
+function Footer(){
+    return <footer className="Footer">
+        <p>&copy; 2023</p>
+    </footer>
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import '../components/Header.css'
+
+function Header(){
+    return <header className="Header">
+        <h1>Transferências</h1>
+    </header>
+}
+
+export default Header;
